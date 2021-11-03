480c462163f43ac682ade41fa93ea3c88082f81a
sergiobarrostm <sergiovictorfb@gmail.com>
2021-11-03 19:02:09 -0300

    spring boot initialzr

---
+++ src/main/java/ufal/ace3comunicaufal/Ace3ComunicaufalApplication.java
@@ -0,0 +1,13 @@
+package ufal.ace3comunicaufal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ace3ComunicaufalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ace3ComunicaufalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufal/ace3comunicaufal/Ace3ComunicaufalApplicationTests.java
@@ -0,0 +1,13 @@
+package ufal.ace3comunicaufal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ace3ComunicaufalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
